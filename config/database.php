b7575deaf7e8294583b3b8153dd75e37c5ba3eee
Dusan Klinec <dusan.klinec@gmail.com>
2017-05-18 15:09:48 +0200

    env, database - use redis for queue

--- config/database.php
+++ config/database.php
@@ -78,6 +78,13 @@ return [
             'prefix' => '',
         ],
 
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'default',
+            'queue' => '{default}',
+            'retry_after' => 90,
+        ],
+
     ],
 
     /*
