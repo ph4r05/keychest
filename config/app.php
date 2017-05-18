3e2369190587eea8ea9536a98765667396700b2d
Dusan Klinec <dusan.klinec@gmail.com>
2017-05-18 16:16:31 +0200

    uuid added

--- config/app.php
+++ config/app.php
@@ -228,6 +228,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Uuid' => Webpatser\Uuid\Uuid::class,
     ],
 
 ];
