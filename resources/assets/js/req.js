3876bb6632d1eea7c6d44865cf43800e6fc12e87
Dusan Klinec <dusan.klinec@gmail.com>
2017-05-30 11:39:44 +0200

    minor formatting change

--- resources/assets/js/req.js
+++ resources/assets/js/req.js
@@ -1,81 +1,78 @@
 /**
  * Created by dusanklinec on 30.05.17.
  */
+'use strict';
 
-
-
-    'use strict';
-
-    /**
-     * Switches main loading overlay.
-     * @param started if true overlay is displayed. Hidden otherwise.
-     */
-    function bodyProgress(started){
-        var htmlBody = $("body");
-        if (started){
-            htmlBody.addClass("loading");
-        } else {
-            htmlBody.removeClass("loading");
-        }
-        console.log('Progress: ' + started);
-        return true;
+/**
+ * Switches main loading overlay.
+ * @param started if true overlay is displayed. Hidden otherwise.
+ */
+function bodyProgress(started){
+    var htmlBody = $("body");
+    if (started){
+        htmlBody.addClass("loading");
+    } else {
+        htmlBody.removeClass("loading");
     }
+    console.log('Progress: ' + started);
+    return true;
+}
 
-    /**
-     * Submit new scan job
-     * @param target
-     * @param onLoaded
-     * @param onFail
-     */
-    function submitJob(target, onLoaded, onFail){
-        $.getJSON("/submitJob", {'scan-target': target})
-            .done(function( json ) {
-                onLoaded(json);
-            })
-            .fail(function( jqxhr, textStatus, error ) {
-                var err = textStatus + ", " + error;
-                console.log( "Request Failed: " + err );
-                onFail(jqxhr, textStatus, error);
-            });
-    }
+/**
+ * Submit new scan job
+ * @param target
+ * @param onLoaded
+ * @param onFail
+ */
+function submitJob(target, onLoaded, onFail){
+    $.getJSON("/submitJob", {'scan-target': target})
+        .done(function( json ) {
+            onLoaded(json);
+        })
+        .fail(function( jqxhr, textStatus, error ) {
+            var err = textStatus + ", " + error;
+            console.log( "Request Failed: " + err );
+            onFail(jqxhr, textStatus, error);
+        });
+}
 
-    /**
-     * Performs call on job current state.
-     * @param uuid
-     * @param onLoaded
-     * @param onFail
-     */
-    function getJobState(uuid, onLoaded, onFail){
-        $.getJSON("/jobState", {'job_uuid': uuid})
-            .done(function( json ) {
-                onLoaded(json);
-            })
-            .fail(function( jqxhr, textStatus, error ) {
-                var err = textStatus + ", " + error;
-                console.log( "Request Failed: " + err );
-                onFail(jqxhr, textStatus, error);
-            });
-    }
+/**
+ * Performs call on job current state.
+ * @param uuid
+ * @param onLoaded
+ * @param onFail
+ */
+function getJobState(uuid, onLoaded, onFail){
+    $.getJSON("/jobState", {'job_uuid': uuid})
+        .done(function( json ) {
+            onLoaded(json);
+        })
+        .fail(function( jqxhr, textStatus, error ) {
+            var err = textStatus + ", " + error;
+            console.log( "Request Failed: " + err );
+            onFail(jqxhr, textStatus, error);
+        });
+}
 
-    /**
-     * Performs call on job results.
-     * @param uuid
-     * @param onLoaded
-     * @param onFail
-     */
-    function getJobResult(uuid, onLoaded, onFail){
-        $.getJSON("/jobResult", {'job_uuid': uuid})
-            .done(function( json ) {
-                onLoaded(json);
-            })
-            .fail(function( jqxhr, textStatus, error ) {
-                var err = textStatus + ", " + error;
-                console.log( "Request Failed: " + err );
-                onFail(jqxhr, textStatus, error);
-            });
-    }
+/**
+ * Performs call on job results.
+ * @param uuid
+ * @param onLoaded
+ * @param onFail
+ */
+function getJobResult(uuid, onLoaded, onFail){
+    $.getJSON("/jobResult", {'job_uuid': uuid})
+        .done(function( json ) {
+            onLoaded(json);
+        })
+        .fail(function( jqxhr, textStatus, error ) {
+            var err = textStatus + ", " + error;
+            console.log( "Request Failed: " + err );
+            onFail(jqxhr, textStatus, error);
+        });
+}
 
-    // Export
+// Export
 module.exports = {
     bodyProgress: bodyProgress,
     submitJob: submitJob,
