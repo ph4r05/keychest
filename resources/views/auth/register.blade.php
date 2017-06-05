6ec88b21e311906ea781a666fa46d3f4dd163817
Dusan Klinec <dusan.klinec@gmail.com>
2017-06-05 18:50:45 +0200

    change merge - register page title

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading">Register now - your dashboard will come out on 9<sup>th</sup> June</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
