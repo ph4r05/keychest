ea9c864b919343cc35afaa7c41305031a407f8d7
Dusan Klinec <dusan.klinec@gmail.com>
2017-06-07 17:15:08 +0200

    lte: minor left panel adjustments to keychest

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -32,16 +32,27 @@
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
             <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
-            <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
-            <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
-            <li class="treeview">
-                <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
-                <ul class="treeview-menu">
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
-                </ul>
+
+            <li class="active">
+                <a href="{{ url('home') }}"><i class='fa fa-dashboard'></i> <span>{{ trans('admin.dashboard') }}</span></a>
             </li>
+
+            <li>
+                <a href="{{ url('home/scan') }}"><i class='fa fa-wpexplorer'></i> <span>{{ trans('admin.scan') }}</span></a>
+            </li>
+
+            <li>
+                <a href="{{ url('home/servers') }}"><i class='fa fa-server'></i> <span>{{ trans('admin.servers') }}</span></a>
+            </li>
+
+            {{--<li class="treeview">--}}
+                {{--<a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>--}}
+                {{--<ul class="treeview-menu">--}}
+                    {{--<li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>--}}
+                    {{--<li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>--}}
+                {{--</ul>--}}
+            {{--</li>--}}
+
         </ul><!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
