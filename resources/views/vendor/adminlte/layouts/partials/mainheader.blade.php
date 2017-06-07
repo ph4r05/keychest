9871fbfa787b893ce5179896f07b7764e4cc0117
Dusan Klinec <dusan.klinec@gmail.com>
2017-06-07 17:38:00 +0200

    lte: keychest admin header

--- resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
@@ -4,9 +4,9 @@
     <!-- Logo -->
     <a href="{{ url('/home') }}" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>A</b>LT</span>
+        <span class="logo-mini"><b>K</b>A</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>Admin</b>LTE Laravel </span>
+        <span class="logo-lg"><b>Keychest</b> Admin</span>
     </a>
 
     <!-- Header Navbar -->
