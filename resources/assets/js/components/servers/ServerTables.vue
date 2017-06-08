9afaeb405690a8ce567a90ab1ce7d5ad45d051ef
Dusan Klinec <dusan.klinec@gmail.com>
2017-06-08 21:22:03 +0200

    servers - bootstrap pagination added

--- resources/assets/js/components/servers/ServerTables.vue
+++ resources/assets/js/components/servers/ServerTables.vue
@@ -12,7 +12,7 @@
       :css="css.table"
       :sort-order="sortOrder"
       :multi-sort="true"
-      :per-page="100"
+      :per-page="50"
       :append-params="moreParams"
       @vuetable:cell-clicked="onCellClicked"
       @vuetable:pagination-data="onPaginationData"
@@ -22,23 +22,26 @@
     <div class="vuetable-pagination">
       <vuetable-pagination-info ref="paginationInfo"
         info-class="pagination-info"
+        :css="css.info"
       ></vuetable-pagination-info>
-      <vuetable-pagination ref="pagination"
+      <vuetable-pagination-bootstrap ref="pagination"
         :css="css.pagination"
-        :icons="css.icons"
         @vuetable-pagination:change-page="onChangePage"
-      ></vuetable-pagination>
+      ></vuetable-pagination-bootstrap>
     </div>
   </div>
 </template>
 <script>
 import accounting from 'accounting';
 import moment from 'moment';
+
+import Vue from 'vue';
+import VueEvents from 'vue-events';
+
 import Vuetable from 'vuetable-2/src/components/Vuetable';
 import VuetablePagination from 'vuetable-2/src/components/VuetablePagination';
 import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo';
-import Vue from 'vue';
-import VueEvents from 'vue-events';
+import VuetablePaginationBootstrap from '../../components/partials/VuetablePaginationBootstrap';
 
 import CustomActions from './CustomActions';
 import DetailRow from './DetailRow';
@@ -58,6 +61,7 @@ export default {
         Vuetable,
         VuetablePagination,
         VuetablePaginationInfo,
+        VuetablePaginationBootstrap
     },
     data () {
         return {
@@ -121,11 +125,14 @@ export default {
                     descendingIcon: 'glyphicon glyphicon-chevron-down'
                 },
                 pagination: {
-                    wrapperClass: 'pagination',
+                    wrapperClass: 'pagination pull-right',
                     activeClass: 'active',
                     disabledClass: 'disabled',
                     pageClass: 'page',
                     linkClass: 'link',
+                },
+                info: {
+                    infoClass: "pull-left"
                 },
                 icons: {
                     first: 'glyphicon glyphicon-step-backward',
@@ -200,6 +207,26 @@ export default {
                     onFail();
                 });
 
+        },
+
+        renderPagination(h) {
+            console.log('pagpag');
+            return h(
+                'div',
+                { class: {'vuetable-pagination': true} },
+                [
+                    h('vuetable-pagination-info', { ref: 'paginationInfo', props: { css: this.css.paginationInfo } }),
+                    h('vuetable-pagination-bootstrap', {
+                        ref: 'pagination',
+                        class: { 'pull-right': true },
+                        props: {
+                        },
+                        on: {
+                            'vuetable-pagination:change-page': this.onChangePage
+                        }
+                    })
+                ]
+            )
         },
     },
     events: {
