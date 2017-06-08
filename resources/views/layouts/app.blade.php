f44f999fffa02a35edb20afef064f9e127600b0e
Dusan Klinec <dusan.klinec@gmail.com>
2017-06-08 18:38:31 +0200

    navigation bar fixed for logged in user, code cleaning

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,16 +70,18 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
+                        <!-- Beta link -->
+                        <li><a href="{{ route('/') }}">Early BETA</a></li>
+
                         <!-- component navigation -->
                         @yield('content-nav')
 
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ route('/') }}">Early BETA</a></li>
-
                             {{--<li><a href="{{ route('login') }}">Login</a></li>--}}
                             <li><a href="{{ route('register') }}">Create account</a></li>
                         @else
+                            <li><a href="{{ route('home') }}">Home</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
