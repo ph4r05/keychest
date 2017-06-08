4dbb925c1344d7293cb13eeb2a78b2a05a1e3bc3
Dusan Klinec <dusan.klinec@gmail.com>
2017-06-08 10:15:05 +0200

    lte: vuetables-2 added

---
+++ resources/assets/js/components/servers/ServerTables.vue
@@ -0,0 +1,204 @@
+<template>
+  <div>
+    <filter-bar></filter-bar>
+    <vuetable ref="vuetable"
+      api-url="http://vuetable.ratiw.net/api/users"
+      :fields="fields"
+      pagination-path=""
+      :css="css.table"
+      :sort-order="sortOrder"
+      :multi-sort="true"
+      detail-row-component="my-detail-row"
+      :append-params="moreParams"
+      @vuetable:cell-clicked="onCellClicked"
+      @vuetable:pagination-data="onPaginationData"
+    ></vuetable> 
+    <div class="vuetable-pagination">
+      <vuetable-pagination-info ref="paginationInfo"
+        info-class="pagination-info"
+      ></vuetable-pagination-info>
+      <vuetable-pagination ref="pagination"
+        :css="css.pagination"
+        :icons="css.icons"
+        @vuetable-pagination:change-page="onChangePage"
+      ></vuetable-pagination>
+    </div>
+  </div>
+</template>
+<script>
+import accounting from 'accounting';
+import moment from 'moment';
+import Vuetable from 'vuetable-2/src/components/Vuetable';
+import VuetablePagination from 'vuetable-2/src/components/VuetablePagination';
+import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo';
+import Vue from 'vue';
+import VueEvents from 'vue-events';
+
+import CustomActions from './CustomActions';
+import DetailRow from './DetailRow';
+import FilterBar from './FilterBar';
+
+Vue.use(VueEvents);
+Vue.component('custom-actions', CustomActions);
+Vue.component('my-detail-row', DetailRow);
+Vue.component('filter-bar', FilterBar);
+
+export default {
+  components: {
+    Vuetable,
+    VuetablePagination,
+    VuetablePaginationInfo,
+  },
+  data () {
+    return {
+      fields: [
+        {
+          name: '__sequence',
+          title: '#',
+          titleClass: 'text-right',
+          dataClass: 'text-right'
+        },
+        {
+          name: '__checkbox',
+          titleClass: 'text-center',
+          dataClass: 'text-center',
+        },
+        {
+          name: 'hostname',
+          sortField: 'hostname',
+        },
+        {
+          name: 'port',
+          sortField: 'port'
+        },
+        {
+          name: 'scheme',
+          sortField: 'scheme'
+        },
+        {
+          name: 'created_at',
+          sortField: 'created_at',
+          titleClass: 'text-center',
+          dataClass: 'text-center',
+          callback: 'formatDate|DD-MM-YYYY'
+        },
+        {
+          name: 'updated_at',
+          sortField: 'updated_at',
+          titleClass: 'text-center',
+          dataClass: 'text-center',
+          callback: 'formatDate|DD-MM-YYYY'
+        },
+        {
+          name: 'last_scan_at',
+          sortField: 'last_scan_at',
+          titleClass: 'text-center',
+          dataClass: 'text-center',
+          callback: 'formatDate|DD-MM-YYYY'
+        },
+        {
+          name: '__component:custom-actions',
+          title: 'Actions',
+          titleClass: 'text-center',
+          dataClass: 'text-center'
+        }
+      ],
+      css: {
+        table: {
+          tableClass: 'table table-bordered table-striped table-hover',
+          ascendingIcon: 'glyphicon glyphicon-chevron-up',
+          descendingIcon: 'glyphicon glyphicon-chevron-down'
+        },
+        pagination: {
+          wrapperClass: 'pagination',
+          activeClass: 'active',
+          disabledClass: 'disabled',
+          pageClass: 'page',
+          linkClass: 'link',
+        },
+        icons: {
+          first: 'glyphicon glyphicon-step-backward',
+          prev: 'glyphicon glyphicon-chevron-left',
+          next: 'glyphicon glyphicon-chevron-right',
+          last: 'glyphicon glyphicon-step-forward',
+        },
+      },
+      sortOrder: [
+        { field: 'email', sortField: 'email', direction: 'asc'}
+      ],
+      moreParams: {}
+    }
+  },
+  methods: {
+    allcap (value) {
+      return value.toUpperCase()
+    },
+    formatNumber (value) {
+      return accounting.formatNumber(value, 2)
+    },
+    formatDate (value, fmt = 'DD-MM-YYYY') {
+      return (value === null) ? '' : moment(value, 'YYYY-MM-DD').format(fmt);
+    },
+    onPaginationData (paginationData) {
+      this.$refs.pagination.setPaginationData(paginationData);
+      this.$refs.paginationInfo.setPaginationData(paginationData);
+    },
+    onChangePage (page) {
+      this.$refs.vuetable.changePage(page);
+    },
+    onCellClicked (data, field, event) {
+      console.log('cellClicked: ', field.name);
+      this.$refs.vuetable.toggleDetailRow(data.id);
+    },
+  },
+  events: {
+    'filter-set' (filterText) {
+      this.moreParams = {
+        filter: filterText
+      };
+      Vue.nextTick( () => this.$refs.vuetable.refresh() )
+    },
+    'filter-reset' () {
+      this.moreParams = {};
+      Vue.nextTick( () => this.$refs.vuetable.refresh() );
+    }
+  }
+}
+</script>
+<style>
+.pagination {
+  margin: 0;
+  float: right;
+}
+.pagination a.page {
+  border: 1px solid lightgray;
+  border-radius: 3px;
+  padding: 5px 10px;
+  margin-right: 2px;
+}
+.pagination a.page.active {
+  color: white;
+  background-color: #337ab7;
+  border: 1px solid lightgray;
+  border-radius: 3px;
+  padding: 5px 10px;
+  margin-right: 2px;
+}
+.pagination a.btn-nav {
+  border: 1px solid lightgray;
+  border-radius: 3px;
+  padding: 5px 7px;
+  margin-right: 2px;
+}
+.pagination a.btn-nav.disabled {
+  color: lightgray;
+  border: 1px solid lightgray;
+  border-radius: 3px;
+  padding: 5px 7px;
+  margin-right: 2px;
+  cursor: not-allowed;
+}
+.pagination-info {
+  float: left;
+}
+</style>
