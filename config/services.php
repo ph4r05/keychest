262391c1bec9a755e3bb023e09e5af539f5b0cea
Dusan Klinec <dusan.klinec@gmail.com>
2017-06-09 11:11:34 +0200

    added all supported social services

--- config/services.php
+++ config/services.php
@@ -55,6 +55,22 @@ return [
         'redirect' => env('LINKEDIN_OAUTH_APP_REDIRECT_URL'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_OAUTH_APP_ID'),
+        'client_secret' => env('GOOGLE_OAUTH_APP_SECRET'),
+        'redirect' => env('GOOGLE_OAUTH_APP_REDIRECT_URL'),
+    ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_OAUTH_APP_ID'),
+        'client_secret' => env('FACEBOOK_OAUTH_APP_SECRET'),
+        'redirect' => env('FACEBOOK_OAUTH_APP_REDIRECT_URL'),
+    ],
+
+    'twitter' => [
+        'client_id' => env('TWITTER_OAUTH_APP_ID'),
+        'client_secret' => env('TWITTER_OAUTH_APP_SECRET'),
+        'redirect' => env('TWITTER_OAUTH_APP_REDIRECT_URL'),
+    ],
 
 ];
