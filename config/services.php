4ff245f03276944107f2e5902ba9bef1f6644004
Dusan Klinec <dusan.klinec@gmail.com>
2017-06-09 10:18:06 +0200

    linked in service added

--- config/services.php
+++ config/services.php
@@ -49,6 +49,12 @@ return [
         'redirect' => env('GITHUB_OAUTH_APP_REDIRECT_URL'),
     ],
 
+    'linkedin' => [
+        'client_id' => env('LINKEDIN_OAUTH_APP_ID'),
+        'client_secret' => env('LINKEDIN_OAUTH_APP_SECRET'),
+        'redirect' => env('LINKEDIN_OAUTH_APP_REDIRECT_URL'),
+    ],
+
 
 
 ];
