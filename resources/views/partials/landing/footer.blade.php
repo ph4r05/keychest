9bf340e9a3c81dfc1348073a20c1a2e5ae193090
Dusan Klinec <dusan.klinec@gmail.com>
2017-06-13 23:00:13 +0200

    terms & policy moved to footer

--- resources/views/partials/landing/footer.blade.php
+++ resources/views/partials/landing/footer.blade.php
@@ -5,7 +5,12 @@
                 <div class="row voffset-lg">
                     <div class="col-sm-10">
                         <p class="mg-md ">
-                            We are&nbsp;<a class="ltc-rich-electric-blue" href="https://enigmabridge.com" target="_blank">Enigma Bridge Ltd</a>, 20 Bridge St, Cambridge, CB2 1UF, United Kingdom and we read keychest@enigmabridge.com
+                            We are&nbsp;<a class="ltc-rich-electric-blue" href="https://enigmabridge.com" target="_blank"
+                            >Enigma Bridge Ltd</a>, 20 Bridge St, Cambridge, CB2 1UF, United Kingdom and we read
+                            keychest@enigmabridge.com
+                            <br/>
+                            <a href="{{ url('/terms') }}">Terms of Service</a>
+                            | <a href="{{ url('/privacy-policy') }}">Privacy Policy</a>
                         </p>
                     </div>
                     <div class="col-sm-2">
