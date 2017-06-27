d296e6d60f6f153b1dfd11f8623e8d6b05c63873
Dusan Klinec <dusan.klinec@gmail.com>
2017-06-27 18:12:37 +0200

    dashboard - intro box removed

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -17,27 +17,28 @@
 			<div class="col-md-8 col-md-offset-2">
 
 				<!-- Default box -->
-				<div class="box">
-					<div class="box-header with-border">
-						<h3 class="box-title">Home</h3>
+				{{--<div class="box">--}}
+					{{--<div class="box-header with-border">--}}
+						{{--<h3 class="box-title">Home</h3>--}}
 
-						<div class="box-tools pull-right">
-							<button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-								<i class="fa fa-minus"></i></button>
-							<button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-								<i class="fa fa-times"></i></button>
-						</div>
-					</div>
-					<div class="box-body">
-						<p>
-							{{ trans('adminlte_lang::message.logged') }} Welcome to Keychest.
-						</p>
-						<p>
-							We are working on a new dashboard, stay tuned.
-						</p>
-					</div>
-					<!-- /.box-body -->
-				</div>
+						{{--<div class="box-tools pull-right">--}}
+							{{--<button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">--}}
+								{{--<i class="fa fa-minus"></i></button>--}}
+							{{--<button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">--}}
+								{{--<i class="fa fa-times"></i></button>--}}
+						{{--</div>--}}
+					{{--</div>--}}
+					{{----}}
+					{{--<div class="box-body">--}}
+						{{--<p>--}}
+							{{--{{ trans('adminlte_lang::message.logged') }} Welcome to Keychest.--}}
+						{{--</p>--}}
+						{{--<p>--}}
+							{{--We are working on a new dashboard, stay tuned.--}}
+						{{--</p>--}}
+					{{--</div>--}}
+					{{--<!-- /.box-body -->--}}
+				{{--</div>--}}
 				<!-- /.box -->
 
 			</div>
