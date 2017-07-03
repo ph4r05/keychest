a6d513aa5a30df98ef495ba2f344449d6c1f04fe
Dusan Klinec <dusan.klinec@gmail.com>
2017-07-03 13:59:51 +0200

    beta link fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,7 +72,11 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Beta link -->
-                        <li><a href="">BETA</a></li>
+                        @if (Auth::guest())
+                            <li><a href="{{ route('register') }}">BETA</a></li>
+                        @else
+                            <li><a href="{{ route('home') }}">BETA</a></li>
+                        @endif
 
                         <!-- component navigation -->
                         @yield('content-nav')
