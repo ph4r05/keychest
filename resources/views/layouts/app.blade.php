01268e6a84a469d46119526408a77297fa1106f8
dancvrcek <dan@enigmabridge.com>
2017-07-04 13:18:40 +0100

    KCH-1 making the main top menu item <b>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,7 +89,7 @@
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    <b>{{ Auth::user()->name }}</b> <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu tc-rich-electric-blue" role="menu">
