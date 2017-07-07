c2e86064ac43d7e6dd3e748fb0f414f4ef488aca
Dusan Klinec <dusan.klinec@gmail.com>
2017-07-07 21:50:07 +0200

    subs: db - models added

--- app/Models/SubdomainWatchAssoc.php
+++ app/Models/SubdomainWatchAssoc.php
@@ -5,16 +5,16 @@ namespace App\Models;
 use App\Keychest\Uuids;
 use Illuminate\Database\Eloquent\Model;
 
-class WatchAssoc extends Model
+class SubdomainWatchAssoc extends Model
 {
     public $incrementing = true;
 
     protected $guarded = array();
 
-    protected $table = 'user_watch_target';
+    protected $table = 'user_subdomain_watch_target';
 
     public function getDates()
     {
-        return array('created_at', 'updated_at', 'deleted_at', 'disabled_at', 'auto_scan_added_at');
+        return array('created_at', 'updated_at', 'deleted_at', 'disabled_at');
     }
 }
