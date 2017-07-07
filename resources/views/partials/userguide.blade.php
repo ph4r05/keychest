0e2403a978edb0a71d5e80352ef87e1d7a861f65
dancvrcek <dan@enigmabridge.com>
2017-07-07 08:58:58 +0100

    KCH-14 user manual - an initial stub

--- resources/views/partials/userguide.blade.php
+++ resources/views/partials/userguide.blade.php
@@ -2,3 +2,41 @@
     We are working on the user guide and FAQ, please stay tuned.
 </p>
 
+<h1>Welcome to KeyChest</h1>
+
+<p>
+    We have designed KeyChest as we need to continuously monitor our servers to avoid any downtimes
+due to expired or misconfigured certificates. We implemented it as a system you setup once and it just
+works.
+</p>
+
+<p>
+We dreamt of several main features to make it a helping tool, rather than just yet another management system
+we have to think about all the time. The best monitoring system is useless, if you forget to register your
+servers with it. So KeyChest will automatically find all new servers the moment you get for them the first
+certificates.
+</p>
+
+
+<h1>How to use KeyChest</h1>
+
+<p>
+    KeyChest gives you a quick information when you are setting up a new server and it continously monitors
+    your certificates and servers. It is not intended for thorough security audits as each would take several
+    minutes to complete and you really want to run those only when the basics are right (server is running,
+    firewall open, TLS setup, certificate installed, etc.). KeyChest helps to quickly complete and verify the
+    initial configuration.
+</p>
+
+<p>
+    KeyChest consists of two main tools:
+    <ul>
+    <li>Spot check - a quick check of a particular server with instant results. A typical use-case for spot check
+    is setup of a new server, or to check correct setup after configuration changes.</li>
+    <li>Dashboard - this tool provides a concise but easy to understand overview of the status of all "watched"
+    servers or internet domains.</li>
+</ul>
+If you want to monitor, you need to import some data into a monitoring system first. There are three options
+add new servers into KeyChest and some more options, if you deploy it on-premise.
+</p>
+
