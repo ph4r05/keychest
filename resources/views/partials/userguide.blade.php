be5dd6c8046d8975954eaa92619fcd141c714e6f
dancvrcek <dan@enigmabridge.com>
2017-07-08 08:04:00 +0100

    KCH-14 still work in progress

--- resources/views/partials/userguide.blade.php
+++ resources/views/partials/userguide.blade.php
@@ -1,24 +1,24 @@
-<p>
-    We are working on the user guide and FAQ, please stay tuned.
-</p>
 
-<h1>Welcome to KeyChest</h1>
+
+<h3 class="mg-md">Welcome to KeyChest</h3>
 
 <p>
     We have designed KeyChest as we need to continuously monitor our servers to avoid any downtimes
-due to expired or misconfigured certificates. We implemented it as a system you setup once and it just
+due to expired or misconfigured certificates. We implemented it as a system you setup and forget. It just
 works.
 </p>
 
 <p>
-We dreamt of several main features to make it a helping tool, rather than just yet another management system
-we have to think about all the time. The best monitoring system is useless, if you forget to register your
-servers with it. So KeyChest will automatically find all new servers the moment you get for them the first
-certificates.
+We had a pretty firm idea about main features to make it a real helping tool, rather than just yet
+    another management system we have to think about all the time (did we add all servers, does data
+    collection work, etc.). The best monitoring system is useless, if you forget to register your
+servers with it. So KeyChest will automatically find all new servers from "Responsive Domains" the moment
+    you prime them with their first certificate. It will keep monitoring servers to detect any downtimes
+    and it will only bother you if there is a problem.
 </p>
 
 
-<h1>How to use KeyChest</h1>
+<h3 class="mg-md">How to use KeyChest</h3>
 
 <p>
     KeyChest gives you a quick information when you are setting up a new server and it continously monitors
@@ -36,7 +36,20 @@ certificates.
     <li>Dashboard - this tool provides a concise but easy to understand overview of the status of all "watched"
     servers or internet domains.</li>
 </ul>
+
+Initially, we thought that Spot checks would be used first - to help you with configuration - and Dashboard
+will take over with its continuous monitoring. The flow changes when you use "Responsive Domains".
+
 If you want to monitor, you need to import some data into a monitoring system first. There are three options
-add new servers into KeyChest and some more options, if you deploy it on-premise.
+to add new servers into KeyChest and some more options, if you deploy it on-premise:
+
+    <ul>
+        <li>Serve name - you simply enter a server name you want to start watching.</li>
+        <li>Responsive domain - you add a domain, for which you want to watch all the subdomains.</li>
+        <li>Bulk import - you can paste a list of domain names - one per line, to speed up initial import.</li>
+    </ul>
+
+On-premise deployments can also scan ranges of private IP addresses.
 </p>
+
 
