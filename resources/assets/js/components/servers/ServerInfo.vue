223fad34e8a9d73a566fa51987e77226962311ff
dancvrcek <dan@enigmabridge.com>
2017-07-09 12:24:22 +0100

    KCH-20 typo

--- resources/assets/js/components/servers/ServerInfo.vue
+++ resources/assets/js/components/servers/ServerInfo.vue
@@ -12,7 +12,7 @@
             <li>in the "Active Domain" tab - servers can be added automatically when you issue
                 the first certificate for them; or
             </li>
-            <li>in the "Bulk Improt" tab - a quicker entry of servers when you start using KeyChest.</li>
+            <li>in the "Bulk Import" tab - a quicker entry of servers when you start using KeyChest.</li>
         </ul>
 
     </help-modal>
