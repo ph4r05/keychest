4917927085f5f4e2b401647be3430c925ac67048
Dusan Klinec <dusan.klinec@gmail.com>
2017-07-10 14:09:52 +0100

    maintenance mode page refactored
    - simple, clean, use our design

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,26 +1,49 @@
-@extends('adminlte::layouts.errors')
+@extends('layouts.app')
 
-@section('htmlheader_title')
-    {{ trans('adminlte_lang::message.serviceunavailable') }}
+@section('content-nav')
 @endsection
 
-@section('main-content')
+@section('content')
+    <div class="bloc bloc-fill-screen tc-onyx bgc-white l-bloc" id="intro" style="height: 400px;">
+        <div class="container">
+            <div class="row">
 
-    <div class="error-page">
-        <h2 class="headline text-red">503</h2>
-        <div class="error-content">
-            <h3><i class="fa fa-warning text-red"></i> Oops! {{ trans('adminlte_lang::message.somethingwrong') }}</h3>
-            <p>
-                {{ trans('adminlte_lang::message.mainwhile') }} <a href='{{ url('/home') }}'>{{ trans('adminlte_lang::message.returndashboard') }}</a> {{ trans('adminlte_lang::message.usingsearch') }}
-            </p>
-            <form class='search-form'>
-                <div class='input-group'>
-                    <input type="text" name="search" class='form-control' placeholder="{{ trans('adminlte_lang::message.search') }}"/>
-                    <div class="input-group-btn">
-                        <button type="submit" name="submit" class="btn btn-danger btn-flat"><i class="fa fa-search"></i></button>
-                    </div>
-                </div><!-- /.input-group -->
-            </form>
+                <div class="col-sm-12">
+                    <img src="/images/logo2-rgb_keychest.png" class="img-responsive center-block" width="300">
+                    <h3 class="text-center mg-lg hero-bloc-text-sub  tc-rich-electric-blue">
+                        We are upgrading our system, be right back.
+                    </h3>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2">
+
+                </div>
+            </div>
+
         </div>
-    </div><!-- /.error-page -->
+
+        <div class="container fill-bloc-bottom-edge">
+            <div class="row row-no-gutters">
+                <div class="col-sm-12">
+                    <a id="scroll-hero" class="blocs-hero-btn-dwn" href="https://keychest.net/#"><span class="fa fa-chevron-down"></span></a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- learn -->
+    @include('partials.landing.learn')
+    <!-- learn END -->
+
+    <!-- ScrollToTop Button -->
+    <a class="bloc-button btn btn-d scrollToTop" onclick="scrollToTarget('1')"><span class="fa fa-chevron-up"></span></a>
+    <!-- ScrollToTop Button END-->
+
+    <!-- Footer - bloc-7 -->
+    @include('partials.landing.footer')
+    <!-- Footer - bloc-7 END -->
+
 @endsection
