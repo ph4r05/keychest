5bc3967d9eba01cd54ec52603021ef8ff4f2e957
dancvrcek <dan@enigmabridge.com>
2017-07-13 23:05:16 +0100

    KCH-21 improving the description of active domains.

--- resources/assets/js/components/subdomains/SubdomainInfo.vue
+++ resources/assets/js/components/subdomains/SubdomainInfo.vue
@@ -2,11 +2,15 @@
     <!-- Subdomain watch modal info -->
     <help-modal id="subdomainsInfoModal" title="Automatic server discovery">
         <p>
-            A unique property of KeyChest allowing it to automatically add new servers in your domains.
+            A unique feature of KeyChest allows it to automatically discover new servers in your Active Domains.
+            For example, let's say you have a domain "company.com" with tens of subdomains. If you want to discover
+            all certificates for those subdomains, you just add "company.com" to the list of Active Domains. The list
+            of servers below will periodically refresh and show all servers, including those with a fresh first certificate.
         </p>
         <p>
             If you enable the Watch Now&trade; feature, KeyChest will automatically add all new servers
-            for monitoring. They will appear in the dashboard with test results on port 443 (HTTPS).
+            for monitoring. Without this option, you have to start monitoring new servers manually for them to appear in
+            the Dashboard. The Watch Now&trade; feature will start testing servers on port 443 (HTTPS).
         </p>
         <p>
             Note: Detection of servers in active domains happens:
