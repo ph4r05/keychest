0b3fec24965640ec7fedfd2b6e7446c9b450080a
Dusan Klinec <dusan.klinec@gmail.com>
2017-08-10 09:37:53 +0200

    KCH-49: remove uneeded polyfills so static analyzer is not confused with missing libs

--- resources/assets/js/polyinit.js
+++ resources/assets/js/polyinit.js
@@ -14,25 +14,25 @@ if (typeof Promise === 'undefined') {
 
 function initPh4Polyfills(options){
 
-    const fillFetch = () => new Promise((resolve) => {
-        if ('fetch' in window) return resolve();
-
-        require.ensure([], () => {
-            require('whatwg-fetch');
-            resolve();
-        }, 'fetch');
-    });
-
-    const fillIntl = () => new Promise((resolve) => {
-        if ('Intl' in window) return resolve();
-
-        require.ensure([], () => {
-            require('intl');
-            require('intl/locale-data/jsonp/en.js');
-
-            resolve();
-        }, 'Intl');
-    });
+    // const fillFetch = () => new Promise((resolve) => {
+    //     if ('fetch' in window) return resolve();
+    //
+    //     require.ensure([], () => {
+    //         require('whatwg-fetch');
+    //         resolve();
+    //     }, 'fetch');
+    // });
+    //
+    // const fillIntl = () => new Promise((resolve) => {
+    //     if ('Intl' in window) return resolve();
+    //
+    //     require.ensure([], () => {
+    //         require('intl');
+    //         require('intl/locale-data/jsonp/en.js');
+    //
+    //         resolve();
+    //     }, 'Intl');
+    // });
 
     const fillCoreJs = () => new Promise((resolve) => {
         if (
@@ -46,7 +46,6 @@ function initPh4Polyfills(options){
         require.ensure([], () => {
             require('core-js');
             console.log('core-js loaded');
-
             resolve();
         }, 'core-js');
     });
