39be99e7d2be31a6a4ef616d6e49ba037e340019
Dusan Klinec <dusan.klinec@gmail.com>
2017-08-10 09:58:14 +0200

    KCH-44: support for active domains limit constant
    TODO: add per-user override

--- config/keychest.php
+++ config/keychest.php
@@ -2,7 +2,8 @@
 
 return [
 
-    'max_servers' => env('APP_MAX_SERVERS', 1000)
+    'max_servers' => env('APP_MAX_SERVERS', 1000),
+    'max_active_domains' => env('APP_ACTIVE_DOMAINS', 1000)
 
 ];
 
