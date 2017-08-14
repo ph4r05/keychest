67c5682e7ae8d2f0800f3d1da7063684d04e5b96
dancvrcek <dan@enigmabridge.com>
2017-08-14 14:59:58 +0100

    KCH-4 text updates - dashboard heading

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -19,7 +19,7 @@ return [
     'enterprise' => "Enterprise",
 
 
-    'dashboard_desc' => '&nbsp;headlines, failures, planning, and useful information',
+    'dashboard_desc' => '&nbsp;headlines, incidents, planning, and useful information',
     'servers_desc' => '&nbsp;manage your servers and domains to watch',
     'scan_desc' => '&nbsp;quickly test a server when you do configuration changes',
     'userguide_desc' => '',
