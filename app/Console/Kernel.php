7bdd309cfe9ad46db51cf321d4d62d7ddcab4131
Dusan Klinec <dusan.klinec@gmail.com>
2017-08-17 15:14:07 +0200

    KCH-58: kernel - no overlaping email tasks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command(CheckCertificateValidityCommand::class)->hourly();
+        // $schedule->command(CheckCertificateValidityCommand::class)->hourly()->withoutOverlapping();
     }
 
     /**
