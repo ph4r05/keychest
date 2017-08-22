8aa5e17bacda6d169452725ed2d4feec3cd0fa28
Dusan Klinec <dusan.klinec@gmail.com>
2017-08-22 17:10:32 +0200

    KCH-68: ws js added

--- resources/views/vendor/adminlte/layouts/partials/scripts.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/scripts.blade.php
@@ -3,6 +3,7 @@
 <!-- JQuery and bootstrap are required by Laravel 5.3 in resources/assets/js/bootstrap.js-->
 <!-- Laravel App -->
 <!-- Compiled app javascript -->
+<script src="{{ url('/ws/socket.io/socket.io.js')  }}"></script>
 <script src="{{ url (mix('/js/manifest.js')) }}"></script>
 <script src="{{ url (mix('/js/vendor.js')) }}"></script>
 <script src="{{ url (mix('/js/polyapp-user.js')) }}"></script>
--- resources/views/vendor/adminlte/layouts/partials/scripts_auth.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/scripts_auth.blade.php
@@ -1,4 +1,5 @@
 <!-- Compiled app javascript -->
+<script src="{{ url('/ws/socket.io/socket.io.js')  }}"></script>
 <script src="{{ url (mix('/js/manifest.js')) }}"></script>
 <script src="{{ url (mix('/js/vendor.js')) }}"></script>
 <script src="{{ url (mix('/js/polyapp-user.js')) }}"></script>
