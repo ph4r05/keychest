98212fbf281f8765c208651833277fd0bbf8eb50
dancvrcek <dan@enigmabridge.com>
2017-08-29 17:06:17 +0100

    KCH-4 update of T&Cs and meta for register & login pages

--- resources/views/partials/terms.blade.php
+++ resources/views/partials/terms.blade.php
@@ -1,4 +1,12 @@
-<p>By using this service, you agree to these terms and conditions:</p>
+
+<p><strong>KeyChest is a certificate expiry and server monitoring for TLS and HTTPS.
+    Enigma Bridge gives you free access to KeyChest at https://keychest.net.
+    KeyChest provides bulk enrollment, as well as continuous discovery of new
+    subdomains. You can access your data via an online dashboard and regular
+    status updates.</strong>
+</p>
+
+<p>By using this free KeyChest service, you agree to the following terms and conditions:</p>
 
 <p><strong>DATA COLLECTION:</strong>
     This service collects data about your activity as well as certificate and
@@ -6,17 +14,28 @@
     other identifiable data beyond what is necessary for the provision of the described services.
 </p>
 
-<p><strong>USE OF EMAIL ADDRESS:</strong>
-    The only personally identifiable information we collect and store is your email address.
-    We only use it to send weekly certificate planning emails with occasional updates
-    regarding our certificate services.
+<p><strong>USE OF PERSONAL DATA:</strong>
+    The primary personally identifiable information we collect and store is your email address.
+    You may be able to enter additional information to enable particular functions of KeyChest.
+    We will use your personal information to provide KeyChest functions. We may also use your
+    personal information to get in touch with updates relevant to KeyChest and/or the service
+    provider.
+</p>
+
+<p><strong>SERVICE PROVIDER:</strong>
+    This service is provided by Enigma Bridge Ltd, registered in England, UK. Postal address is
+    20 Bridge Street, CB2 1UF Cambridge, United Kingdom. The electronic contact to the service
+    provider as <a href="mailto:support@enigmabridge.com">support@enigmabridge.com</a> .
 </p>
 
 <p><strong>LIABILITY:</strong>
-    You use our services at your own risk. Our services are provided on an 'as is' and
-    'as available' basis. We are not liable for damages, direct or
-    consequential, resulting from any failure to provide service, suspension
-    of service, or termination of service. We do not guaranty the availability
-    of the services. You agree not to hold us responsible for data loss or
-    interruption of service of any kind.
+    You use services provided at https://keychest.net at your own risk. All the services at
+    https://keychest.net are provided on an 'as is' and 'as available' basis.
+    We are not liable for damages, direct or consequential, resulting from any failure to
+    provide service, suspension of service, or termination of service. We do not guaranty
+    the availability of the services. You agree not to hold us responsible for data loss or
+    interruption of service of any kind. <br><br>
+    If you want to use KeyChest for your production or critical systems and, send a request to
+    the service provider as it offers subscriptions with well-defined service layer agreements
+    (SLA).
 </p>
