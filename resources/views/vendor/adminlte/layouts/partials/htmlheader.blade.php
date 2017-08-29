cab74851ec1abbcaf0251d87bcb7b01da79757de
dancvrcek <dan@enigmabridge.com>
2017-08-29 17:06:17 +0100

    KCH-4 update of T&Cs and meta for register & login pages

--- resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
@@ -4,6 +4,32 @@
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <link rel="shortcut icon" type="image/png" href="/images/favicon.png">
 
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="KeyChest - certificate expiry, certificate monitoring for TLS, HTTPS, Letsencrypt, with free cloud service. Automatic monitoring of subdomain servers as they are set up.">
+    <meta name="author" content="Enigma Bridge Ltd, KeyChest">
+    <meta name="keywords" content="letsencrypt, monitor, monitoring, tls, https, certificate expiry, certificate expiration, certificate monitoring, free monitoring">
+    <meta name="title" content="Certificate monitoring KeyChest for HTTPS, TLS, Letsencrypt expiry and status">
+
+    <!-- Facebook -->
+    <meta property="og:title" content="KeyChest certificate monitoring - HTTPS, TLS, Letsencrypt" >
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="KeyChest - certificate expiry, certificate monitoring for TLS, HTTPS, Letsencrypt, with free cloud service. Automatic monitoring of subdomain servers as they are set up." >
+    <meta property="og:url" content="{{ url('/home') }}" />
+    <meta property="og:image" content="{{url('/images/keychest_centre_meta_clean.png') }}" />
+
+    <!-- Twitter -->
+    <meta name="twitter:card" content="landing_centre_clean">
+
+    <meta name="twitter:title" content="KeyChest certificate monitoring - HTTPS, TLS, Letsencrypt">
+    <meta name="twitter:description" content="KeyChest - certificate expiry, certificate monitoring  for TLS, HTTPS, Letsencrypt, with free cloud service. Automatic monitoring of subdomain servers as they are set up.">
+    <meta name="twitter:image" content="{{url('/images/keychest_centre_meta_clean.png') }}">
+
+
+
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -50,7 +76,7 @@
     <!-- Google Analytics -->
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
