4f621889458743de033a64ba22f1fdbaf57bf1c1
Dusan Klinec <dusan.klinec@gmail.com>
2017-09-03 02:11:13 +0200

    KCH-67: config file initializer command added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\CheckCertificateValidityCommand;
+use App\Console\Commands\Setup;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        CheckCertificateValidityCommand::class
+        CheckCertificateValidityCommand::class,
+        Setup::class,
     ];
 
     /**
