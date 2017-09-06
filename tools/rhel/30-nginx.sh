181cdd1339e897923070cd52bbcbe27212ed717c
Dusan Klinec <dusan.klinec@gmail.com>
2017-09-06 16:59:08 +0200

    KCH-67: nginx config script fix

--- tools/rhel/30-nginx.sh
+++ tools/rhel/30-nginx.sh
@@ -8,12 +8,12 @@ export NGINX_HTTP_CFG=/etc/nginx/conf.d/keychest.conf
 export NGINX_HTTPS_CFG=/etc/nginx/conf.d/keychest-tls.conf
 
 # nginx domain names
-sudo sed -i "s/server_name \(.\+\);\$/server_name \1 $KC_DOMAIN;/g" $NGINX_HTTP_CFG
-sudo sed -i "s/server_name \(.\+\);\$/server_name \1 $KC_DOMAIN;/g" $NGINX_HTTPS_CFG
+sudo sed -i "s/server_name \(.\+\);\$/server_name \1 ${KC_DOMAIN};/g" $NGINX_HTTP_CFG
+sudo sed -i "s/server_name \(.\+\);\$/server_name \1 ${KC_DOMAIN};/g" $NGINX_HTTPS_CFG
 
 # nginx certificate
-sudo sed -i "s#ssl_certificate \(.\+\);\$#ssl_certificate $KC_CERT_CHAIN#g" $NGINX_HTTPS_CFG
-sudo sed -i "s#ssl_certificate_key \(.\+\);\$#ssl_certificate_key $KC_CERT_PRIV#g" $NGINX_HTTPS_CFG
+sudo sed -i "s#ssl_certificate \(.\+\);\$#ssl_certificate ${KC_CERT_CHAIN};#g" $NGINX_HTTPS_CFG
+sudo sed -i "s#ssl_certificate_key \(.\+\);\$#ssl_certificate_key ${KC_CERT_PRIV};#g" $NGINX_HTTPS_CFG
 
 # nginx hash bucket size
 echo 'server_names_hash_bucket_size 128;' | sudo tee /etc/nginx/conf.d/01-nginx.conf
