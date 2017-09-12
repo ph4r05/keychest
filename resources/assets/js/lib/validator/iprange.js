a2839aa428593dc986d153eb700565dbe6e9fef0
Dusan Klinec <dusan.klinec@gmail.com>
2017-09-12 13:44:00 +0200

    KCH-77: ip range validator - range size, start <= end

--- resources/assets/js/lib/validator/iprange.js
+++ resources/assets/js/lib/validator/iprange.js
@@ -1,4 +1,6 @@
 import isIP from 'validator/lib/isIP';
+import ipaddr from 'ipaddr.js';
+import ph4 from '../ph4';
 
 export default {
     getMessage(field, args, data) {
@@ -10,15 +12,19 @@ export default {
         if (matchType === 1){
             if (code === 1){
                 return 'The IP range start is not valid IP address';
-            } else {
+            } else if (code === 2) {
                 return 'The IP range stop is not valid IP address';
+            } else if (code === 3){
+                return 'The IP range start address is greater than IP range stop address';
             }
         }
 
         if (code === 1){
             return 'The IP range start is not valid IP address';
-        } else {
+        } else if (code === 2) {
             return 'The IP range size is not valid';
+        } else if (code === 4) {
+            return 'The IP range size is too big';
         }
     },
     validate(value, args) {
@@ -42,6 +48,16 @@ export default {
                 return {valid: false, data: [1,2]};
             }
 
+            const ip1 = ipaddr.IPv4.parse(m1[1]).toByteArray();
+            const ip2 = ipaddr.IPv4.parse(m1[2]).toByteArray();
+            if (ph4.arr_cmp(ip1, ip2) === 1){
+                return {valid: false, data: [1,3]};
+            }
+
+            if (args && ph4.ip_range(ip1, ip2) > args[0]){
+                return {valid: false, data: [2,4]};
+            }
+
             return {valid: true, data: [1,0]};
 
         } else {
@@ -52,6 +68,10 @@ export default {
             const range = +m2[2];
             if(range<0 || range>32){
                 return {valid: false, data: [2,2]};
+            }
+
+            if (args && 2**(32-range) > args[0]){
+                return {valid: false, data: [2,4]};
             }
 
             return {valid: true, data: [2,0]};
