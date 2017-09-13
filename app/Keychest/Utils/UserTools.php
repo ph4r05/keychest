12906218a72b7ce85e4ce7076b9689f2d3ad894d
Dusan Klinec <dusan.klinec@gmail.com>
2017-09-13 19:14:05 +0200

    KCH-105: generating new tokens on user registration

--- app/Keychest/Utils/UserTools.php
+++ app/Keychest/Utils/UserTools.php
@@ -10,6 +10,7 @@ namespace App\Keychest\Utils;
 
 use App\User;
 use Illuminate\Support\Collection;
+use RandomLib\Factory;
 
 /**
  * User tools
@@ -19,6 +20,8 @@ use Illuminate\Support\Collection;
  */
 class UserTools {
 
+    const TOKEN_CHARSET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
     /**
      * Generates accredit value
      * @param User $user
@@ -26,5 +29,23 @@ class UserTools {
      */
     public static function accredit($user){
         return empty($user) ? null : substr(md5($user->id . ':' . $user->email), 0, 24);
+    }
+
+    /**
+     * Email verification token
+     * @param $user
+     * @return string
+     */
+    public static function generateVerifyToken($user){
+        return (new Factory())->getLowStrengthGenerator()->generateString(24, self::TOKEN_CHARSET);
+    }
+
+    /**
+     * Unsubscribe token
+     * @param $user
+     * @return string
+     */
+    public static function generateUnsubscribeToken($user){
+        return (new Factory())->getLowStrengthGenerator()->generateString(24, self::TOKEN_CHARSET);
     }
 }
--- app/Listeners/OnUserRegistered.php
+++ app/Listeners/OnUserRegistered.php
@@ -26,6 +26,8 @@ class OnUserRegistered
     public function handle(Registered $event)
     {
         $event->user->accredit_own = UserTools::accredit($event->user);
+        $event->user->email_verify_token = UserTools::generateVerifyToken($event->user);
+        $event->user->weekly_unsubscribe_token = UserTools::generateUnsubscribeToken($event->user);
         $event->user->save();
     }
 }
