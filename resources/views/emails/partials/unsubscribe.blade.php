13e8c80299d7e61f37d23258123cdcfb93764d48
dancvrcek <dan@enigmabridge.com>
2017-09-15 01:58:20 +0200

    KCH-128 more unsubscribe text edits

--- resources/views/emails/partials/unsubscribe.blade.php
+++ resources/views/emails/partials/unsubscribe.blade.php
@@ -1,2 +1,5 @@
-Click for <a href="{{ url('unsubscribe/' . $user->weekly_unsubscribe_token) }}">unsubscribe</a>. <br/>
-To manage more KeyChest settings go to the <a href="{{ url('home/license') }}">account settings</a> in KeyChest.
+Weekly status emails are now being sent only if we find a certificate, which needs to be renewed in the next 28 days.
+You can use <a href="{{ url('unsubscribe/' . $user->weekly_unsubscribe_token) }}">this link to stop receiving weekly
+emails</a>. <br/>
+If you change your mind later, you can adjust this and other account options in your
+<a href="{{ url('home/license') }}">KeyChest dashboard</a>.
