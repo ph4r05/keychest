6adb12bbd7bd7456a8643984a17d363144d97a1e
Dusan Klinec <dusan.klinec@gmail.com>
2017-09-21 22:20:47 +0200

    KCH-72: api key self registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers;
 use App\Keychest\Services\EmailManager;
 use App\Keychest\Services\LicenseManager;
 use App\Keychest\Services\UserManager;
+use App\Models\ApiKey;
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Log;
+use App\Keychest\Services\Results\ApiKeySelfRegistrationResult;
 
 
 /**
@@ -107,12 +110,41 @@ class UserController extends Controller
      */
     public function claimAccess(Request $request){
         $this->validate($request, [
-            'email' => 'bail|required|email',
+            'email' => 'bail|required|email|max:255',
             'api_key' => 'bail|required|max:64'
         ]);
 
-        
+        $bailResponse = response()->json(['status' => 'not-allowed'], 405);
+        $successResponse = response()->json(['status' => 'success'], 200);
 
-        return response()->json(['status' => 'success'], 200);
+        $email = Input::get('email');
+        $apiKey = Input::get('api_key');
+
+        // Load user by the email, user has to exist before this call.
+        $user = User::query()->where('email', '=', $email)->first();
+        if (empty($user)){
+            Log::debug('User with email not found: ' . $email);
+            return $bailResponse;
+        }
+
+        // Check if there is such api code already. If exists, return success.
+        // If the code is revoked, return not-allowed.
+        $apiKeyObj = $user->apiKeys()->where('api_key', '=', $apiKey)->first();
+        if ($apiKeyObj){
+            Log::debug('User ${email} has existing api key ${apiKey}');
+            return empty($apiKeyObj->revoked_at) ? $successResponse : $bailResponse;
+        }
+
+        // Check if the api key self registration is allowed (block, blacklist, denial, too many)
+        $newApiAllowedStatus = $this->userManager->newApiKeySelfRegistrationAllowed($user, $request);
+        if ($newApiAllowedStatus == ApiKeySelfRegistrationResult::$TOO_MANY){
+            return response()->json(['status' => 'too-many'], 405);
+        } elseif ($newApiAllowedStatus != ApiKeySelfRegistrationResult::$ALLOWED){
+            return $bailResponse;
+        }
+
+        // Create a new API key if the user has allowed it.
+        $this->userManager->registerNewApiKey($user, $apiKey, $request);
+        return response()->json(['status' => 'created'], 200);
     }
 }
--- app/Keychest/Services/UserManager.php
+++ app/Keychest/Services/UserManager.php
@@ -8,11 +8,14 @@
 
 namespace App\Keychest\Services;
 
+use App\Keychest\Services\Results\ApiKeySelfRegistrationResult;
+use App\Models\ApiKey;
 use App\User;
 use Carbon\Carbon;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
+
 
 class UserManager {
 
@@ -31,6 +34,65 @@ class UserManager {
     public function __construct($app)
     {
         $this->app = $app;
+    }
+
+    /**
+     * Returns true if the user new api key self registration is allowed for this request
+     * @param $user
+     * @param $request
+     * @return int
+     */
+    public function newApiKeySelfRegistrationAllowed($user, $request)
+    {
+        // Global app switch
+        if (!config('keychest.enabled_api_self_register')){
+            return ApiKeySelfRegistrationResult::$GLOBAL_DENIAL;
+        }
+
+        // TODO: global switch
+        // TODO: ip blacklist
+        // TODO: rate limiting
+
+        // Number of non-confirmed non-revoked requests - limit.
+        $cnt = $this->getNonApprovedApiKeys($user)->count();
+        if ($cnt > 100){
+            return ApiKeySelfRegistrationResult::$TOO_MANY;
+        }
+
+        return ApiKeySelfRegistrationResult::$ALLOWED;
+    }
+
+    /**
+     * Returns query on the api keys non-revoked, non-approved yet for the user.
+     * @param $user
+     * @return mixed
+     */
+    public function getNonApprovedApiKeys($user){
+        return $user->apiKeys()->whereNull('revoked_at')->whereNull('verified_at');
+    }
+
+    /**
+     * Registers a new Api key for the usage.
+     * No privilege checking is done. All checks should be done before calling this function.
+     *
+     * @param $user
+     * @param $apiKey
+     * @param $request
+     * @return ApiKey
+     */
+    public function registerNewApiKey($user, $apiKey, $request){
+        $apiObj = new ApiKey([
+            'name' => 'self-registered',
+            'api_key' => $apiKey,
+            'email_claim' => $user->email,
+            'ip_registration' => $request->ip(),
+            'user_id' => $user->id,
+            'last_seen_active_at' => Carbon::now(),
+            'last_seen_ip' => $request->ip()
+        ]);
+
+        $user->apiKeys()->save($apiObj);
+        return $apiObj;
     }
 
 
