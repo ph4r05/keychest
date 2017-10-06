962f7dc2d0b45cf99bc012b1496f02bdbb4d838a
Dusan Klinec <dusan.klinec@gmail.com>
2017-10-06 15:51:32 +0200

    KCH-133: tpl page footer added

--- resources/views/vendor/adminlte/layouts/landing-tpl.blade.php
+++ resources/views/vendor/adminlte/layouts/landing-tpl.blade.php
@@ -11,6 +11,18 @@
         @yield('content')
     </div><!-- ./wrapper -->
 </div>
+
+<!-- Main Footer -->
+<footer class="main-footer" style="margin-left: 0">
+    <div class="pull-right hidden-xs">
+    </div>
+    <strong>Copyright &copy; 2017 <a href="http://enigmabridge.com">enigmabridge.com</a>.</strong>
+
+    <div class="ph4-modal">
+        <div class="ph4-modal-wrap"></div>
+    </div>
+</footer>
+
 @section('scripts')
     @include('adminlte::layouts.partials.scripts')
 @show
