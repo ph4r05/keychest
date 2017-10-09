3b993650dbbc97b6746bdce30b37ba4d4f175aba
Dusan Klinec <dusan.klinec@gmail.com>
2017-10-09 14:43:19 +0200

    KCH-105: php 7.1 install instructions fix

--- tools/rhel/02-php71-amazon.sh
+++ tools/rhel/02-php71-amazon.sh
@@ -4,33 +4,34 @@
 # https://rpms.remirepo.net/enterprise/remi-release-7.rpm
 
 sudo rpm -Uvh https://rpms.remirepo.net/enterprise/remi-release-6.rpm
-yum-config-manager --enable remi-php71
+sudo yum-config-manager --enable remi-php71
 
-yum localinstall http://vault.centos.org/6.5/SCL/x86_64/scl-utils/scl-utils-20120927-8.el6.centos.alt.x86_64.rpm
+sudo yum localinstall -y http://vault.centos.org/6.5/SCL/x86_64/scl-utils/scl-utils-20120927-8.el6.centos.alt.x86_64.rpm
 
-yum install php71 php71-fpm php71-mysqlnd php71-mbstring php71-gd php71-xml \
-php71-pecl-xdebug php71-opcache php71-intl \
-php71-pear php71-pecl-redis
-
-# ------------------------------
-# OLD - not finished
-#
+sudo yum install php71 php71-fpm php71-mysqlnd php71-mbstring php71-gd php71-xml \
+    php71-pecl-xdebug php71-opcache php71-intl \
+    php71-pear php71-pecl-redis
 
 # change apache to nginx
 # /etc/php-fpm-7.1.d/www.conf
 sudo sed -i 's/user = apache/user = nginx/g' /etc/php-fpm-7.1.d/www.conf
 sudo sed -i 's/group = apache/group = nginx/g' /etc/php-fpm-7.1.d/www.conf
 
-sudo systemctl enable php71-fpm.service
-sudo systemctl start php71-fpm.service
-sudo systemctl status php71-fpm.service
+sudo chkconfig php-fpm-7.1 --level=345 on
+sudo /etc/init.d/php-fpm-7.1 start
+sudo /etc/init.d/php-fpm-7.1 status
+
+# Amazon linux is RHEL 6 based, so no systemcl
+# sudo systemctl enable php71-fpm.service
+# sudo systemctl start php71-fpm.service
+# sudo systemctl status php71-fpm.service
 
 # Composer
 cd /tmp
-source /etc/profile.d/php.sh
-
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-sudo /opt/rh/rh-php56/root/bin/php composer-setup.php --install-dir=/bin --filename=composer
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo php composer-setup.php --install-dir=/bin --filename=composer
 
 
