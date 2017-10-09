ff92cd385bd1f350e55d6082ebfe085852f3d273
Dusan Klinec <dusan.klinec@gmail.com>
2017-10-09 14:45:01 +0200

    KCH-154: pyenv python install instructions added

---
+++ tools/rhel/04-python-pyenv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo git clone https://github.com/pyenv/pyenv.git /opt/pyenv
+sudo chown $KC_USER /opt/pyenv
+
+echo 'export PYENV_ROOT="/opt/pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+pyenv install 2.7.14
+pyenv install 3.6.3
+pyenv local 2.7.14
+
+echo 'export PATH=/usr/local/bin:$PATH' | sudo tee /etc/profile.d/py2.7.13.sh
+source /etc/profile.d/py2.7.13.sh
+
+# In case of a problem use pip --cert /etc/pki/ca-trust/source/anchors/bcoatpip.pym
+# to workaround problem with corporate proxy, add your certs to the verify paths
+python -c "import ssl; print(ssl.get_default_verify_paths())"
+
+# Make sure you setup CAs as described in node-corp.sh
+export REQUESTS_CA_BUNDLE=/etc/pki/tls/cert.pem
+echo 'REQUESTS_CA_BUNDLE=/etc/pki/tls/cert.pem' >> /etc/environment
+
+# If that does not help:
+# find: requests/cacert.pem
+# find: cacerts.txt
+
