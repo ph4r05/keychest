b7b60e8c0482a8ae586f947ea33e72fed3f47abe
dancvrcek <dan@enigmabridge.com>
2017-10-12 17:54:57 +0100

    KCH-133 minor email text update

--- resources/views/emails/keytest/report.blade.php
+++ resources/views/emails/keytest/report.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # ROCA Key Vulnerability Report
 
-We have completed a test of an email signing key with the following result.
+We have completed a test of your email signing key with the following definitive result.
 
 @component('mail::panel', ['type' => $allSafe ? 'success' : 'danger'])
 @if($allSafe)
@@ -20,8 +20,8 @@ We identified some issues with your keys.
 | ------------  | ------- |
 | Key ID        | {{ $key->keyId }} |
 | Master key ID | {{ $key->masterKeyId }} |
-| Created at    | {{ $key->createdAt ? $key->createdAt->toFormattedDateString() : '' }} |
-| Bit Size      | {{ $key->bitSize }} |
+| Created on    | {{ $key->createdAt ? $key->createdAt->toFormattedDateString() : '' }} |
+| Bit length    | {{ $key->bitSize }} |
 | Test result   | *{{ $key->verdict }}* |
 
 @endcomponent
@@ -37,16 +37,16 @@ We identified some issues with your keys.
 | ------------  | ------- |
 | Subject       | {{ $key->subject }} |
 | Issuer        | {{ $key->issuerOrg }} |
-| Created at    | {{ $key->createdAt ? $key->createdAt->toFormattedDateString() : '' }} |
+| Created on    | {{ $key->createdAt ? $key->createdAt->toFormattedDateString() : '' }} |
 | Valid until   | {{ $key->notValidAfter ? $key->notValidAfter->toFormattedDateString() : '' }} |
-| Bit Size      | {{ $key->bitSize  }} |
+| Bit length    | {{ $key->bitSize  }} |
 | Test result   | *{{ $key->status != 'ok' ?  'Processing error' : ($key->marked ? 'Vulnerable' : 'Safe') }}* |
 
 @endcomponent
 @endforeach
 @endif
 
-Thank you for using our KeyChest key check service. The check was performed {{ $checkTime->toFormattedDateString() }}.
+The key test was performed {{ $checkTime->toFormattedDateString() }} by certificate management system KeyChest.
 
 @component('mail::button', ['url' => route('tester')])
 More information
