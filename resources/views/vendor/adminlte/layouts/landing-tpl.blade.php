bc5e57cc5d44a060aed5c7e6cdbd5f922962110d
Dusan Klinec <dusan.klinec@gmail.com>
2017-10-13 18:03:05 +0200

    KCH-133: main template for roca restructured, footer in cloak, spinner out
    - better UX

--- resources/views/vendor/adminlte/layouts/landing-tpl.blade.php
+++ resources/views/vendor/adminlte/layouts/landing-tpl.blade.php
@@ -5,23 +5,23 @@
     @include('adminlte::layouts.partials.htmlheader')
 @show
 
-<body class="skin-blue-light sidebar-mini">
-<div id="app" v-cloak>
+<body class="skin-blue-light sidebar-mini main-vue-loading">
+<div id="app">
     <div class="wrapper">
         @yield('content')
     </div><!-- ./wrapper -->
+
+    <!-- Main Footer -->
+    <footer class="main-footer" style="margin-left: 0">
+        <div class="pull-right hidden-xs">
+        </div>
+        <strong>Copyright &copy; 2017 <a href="http://enigmabridge.com">enigmabridge.com</a>.</strong>
+    </footer>
 </div>
 
-<!-- Main Footer -->
-<footer class="main-footer" style="margin-left: 0">
-    <div class="pull-right hidden-xs">
-    </div>
-    <strong>Copyright &copy; 2017 <a href="http://enigmabridge.com">enigmabridge.com</a>.</strong>
-
-    <div class="ph4-modal">
-        <div class="ph4-modal-wrap"></div>
-    </div>
-</footer>
+<div class="ph4-modal">
+    <div class="ph4-modal-wrap"></div>
+</div>
 
 @section('scripts')
     @include('adminlte::layouts.partials.scripts')
