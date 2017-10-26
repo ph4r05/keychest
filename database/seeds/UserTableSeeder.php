2b0e47b33b04fbd1bfc9e900de284920a7e3ddd5
Dusan Klinec <dusan.klinec@gmail.com>
2017-10-26 17:31:06 +0200

    KCH-161: database seeder for users extended

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UserTableSeeder::class);
+        factory(App\Models\User::class, 2)->create()->each(function($u) {
+            
+        });
     }
 }
