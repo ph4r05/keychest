0ab13fc3455fe77007a700aaf301550488a30137
Dusan Klinec <dusan.klinec@gmail.com>
2017-11-09 15:56:05 +0100

    KCH-174: abbreviation fix for collapsed sidebar

--- resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
@@ -4,7 +4,7 @@
     <!-- Logo -->
     <a href="{{ url('/home') }}" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>K</b>A</span>
+        <span class="logo-mini"><b>K</b>D</span>
         <!-- logo for regular state and mobile devices -->
         <span class="logo-lg"><b>KeyChest</b> Dashboard</span>
     </a>
