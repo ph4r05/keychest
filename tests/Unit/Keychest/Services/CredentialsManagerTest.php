a647a12465583ac4af7b866527757c51fdad1d4e
Dusan Klinec <dusan.klinec@gmail.com>
2017-11-10 14:33:40 +0100

    KCH-161: test for SSH key generator added

---
+++ tests/Unit/Keychest/Services/CredentialsManagerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit\Keychest\Services;
+
+use App\Keychest\Services\CredentialsManager;
+use Tests\CreatesApplication;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CredentialsManagerTest extends TestCase
+{
+    use CreatesApplication;
+
+    protected function setUp()
+    {
+        $this->app = $this->createApplication();
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testRsaKeyGenerator()
+    {
+        $this->assertTrue(true);
+        $cmag = $this->app->make(CredentialsManager::class);
+
+        $genKey = $cmag->generateSshKey(1024);
+        $this->assertNotNull($genKey);
+
+        $this->assertNotNull($genKey->getPrivateKey());
+        $this->assertNotNull($genKey->getPublicKey());
+        $this->assertStringStartsWith('ssh-rsa ', $genKey->getPublicKey());
+        $this->assertContains('BEGIN RSA PRIVATE KEY', $genKey->getPrivateKey());
+    }
+}
