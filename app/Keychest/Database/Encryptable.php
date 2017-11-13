df0cefe266a3e8ff1b9d5e04c91f6e28a534dd5c
Dusan Klinec <dusan.klinec@gmail.com>
2017-11-13 15:28:44 +0100

    KCH-161: encryptable trait

---
+++ app/Keychest/Database/Encryptable.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dusanklinec
+ * Date: 13.11.17
+ * Time: 15:15
+ */
+
+namespace App\Keychest\Database;
+
+/**
+ * Encryptable Eloquent attributes with app password and standard Crypt facade.
+ *
+ * Usage:
+ *
+ * <code>
+ * use Encryptable;
+ *
+ * protected $encryptable = [
+ *      'attr1',
+ *      'attr2
+ * ];
+ *
+ * </code>
+ *
+ * Trait Encryptable
+ */
+trait Encryptable
+{
+    public function getAttribute($key)
+    {
+        $value = parent::getAttribute($key);
+
+        if (in_array($key, $this->encryptable)) {
+            $value = Crypt::decrypt($value);
+        }
+
+        return $value;
+    }
+
+    public function setAttribute($key, $value)
+    {
+        if (in_array($key, $this->encryptable)) {
+            $value = Crypt::encrypt($value);
+        }
+
+        return parent::setAttribute($key, $value);
+    }
+}
---
+++ app/Keychest/Database/EncryptableGcm.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dusanklinec
+ * Date: 13.11.17
+ * Time: 15:16
+ */
+
+namespace App\Keychest\Database;
+
+/**
+ * Encryptable Eloquent attributes with PBKDF2 + AES-GCM scheme.
+ *
+ * TODO: implement PBKDF2 salting with custom Encrypter: @see \Illuminate\Encryption\Encrypter
+ *
+ * Trait EncryptableGcm
+ */
+trait EncryptableGcm
+{
+    public function getAttribute($key)
+    {
+        $value = parent::getAttribute($key);
+
+        if (in_array($key, $this->encryptable)) {
+            $value = Crypt::decrypt($value);
+        }
+
+        return $value;
+    }
+
+    public function setAttribute($key, $value)
+    {
+        if (in_array($key, $this->encryptable)) {
+            $value = Crypt::encrypt($value);
+        }
+
+        return parent::setAttribute($key, $value);
+    }
+}
