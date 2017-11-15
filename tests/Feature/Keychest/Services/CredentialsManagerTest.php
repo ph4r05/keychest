d9b83b5e8b5c568f7462b15a2b99a4d39e1d8f32
Dusan Klinec <dusan.klinec@gmail.com>
2017-11-15 21:05:25 +0100

    KCH-161: credentials manager feature test, db storage, field encryption test

---
+++ tests/Feature/Keychest/Services/CredentialsManagerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature\Keychest\Services;
+
+use App\Keychest\Services\CredentialsManager;
+use App\Models\SshKey;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Tests\CreatesApplication;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CredentialsManagerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testSshKeyStorage()
+    {
+        $bitSize = 1024;
+        $credentialsManager = $this->app->make(CredentialsManager::class);
+        $sshKey = $credentialsManager->generateSshKey($bitSize);
+        $sshKeyDb = $credentialsManager->generateSshPoolKey($sshKey);
+
+        $this->assertEquals($sshKey->getPublicKey(), $sshKeyDb->pub_key);
+        $this->assertEquals($sshKey->getPrivateKey(), $sshKeyDb->priv_key);
+        $this->assertEquals($bitSize, $sshKeyDb->bit_size);
+        $this->assertEquals(0, $sshKeyDb->rec_version);
+        $this->assertNotNull($sshKeyDb->key_id);
+
+        $loadedElo = SshKey::query()
+            ->where('key_id', '=', $sshKeyDb->key_id)
+            ->get()->first();
+
+        $this->assertNotNull($loadedElo);
+        $this->assertEquals($sshKey->getPrivateKey(), $loadedElo->priv_key);
+
+        // Test if DB encryption works
+        $this->assertNotNull($loadedElo->getAttributes()['priv_key']);
+        $this->assertStringStartsWith('{', $loadedElo->getAttributes()['priv_key']);
+        $this->assertContains('"scheme"', $loadedElo->getAttributes()['priv_key']);
+        $this->assertNotContains($sshKey->getPrivateKey(), $loadedElo->getAttributes()['priv_key']);
+
+        $loaded = DB::table(SshKey::TABLE)
+            ->selectRaw('pub_key, priv_key, priv_key AS privk')
+            ->where('key_id', '=', $sshKeyDb->key_id)
+            ->get()->first();
+
+        $this->assertNotNull($loaded);
+        $this->assertNotEmpty($loaded->priv_key);
+        $this->assertNotEmpty($loaded->privk);
+        $this->assertNotEquals($sshKey->getPrivateKey(), $loaded->privk);
+        $this->assertEquals($loadedElo->getAttributes()['priv_key'], $loaded->privk);
+    }
+}
