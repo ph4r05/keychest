81388ae9581b2c3f707b32fb4fcc2d2110964b26
Dusan Klinec <dusan.klinec@gmail.com>
2017-12-04 10:44:29 +0100

    KCH-161: cert list processor - OV added

--- app/Keychest/Utils/Pricelist/CertificatePriceListProcessor.php
+++ app/Keychest/Utils/Pricelist/CertificatePriceListProcessor.php
@@ -105,6 +105,7 @@ class CertificatePriceListProcessor
 
         $certificate->rule_id = $rule->id;
         $crt_ev = $certificate->is_ev;
+        $crt_ov = $certificate->is_ov;
         $crt_wild = $certificate->is_cn_wildcard || $certificate->is_alt_wildcard;
 
         if ($crt_ev && $crt_wild) {
@@ -133,6 +134,31 @@ class CertificatePriceListProcessor
                 $certificate->price_approx = true;
             }
 
+        } elseif ($crt_ov && $crt_wild){
+            if ($rule->price_ov_wildcard){
+                $certificate->price = $rule->price_ov_wildcard;
+
+            } elseif ($rule->price_ov){
+                $certificate->price = $rule->price_ov;
+                $certificate->price_approx = true;
+
+            } elseif ($rule->price_wildcard){
+                $certificate->price = $rule->price_wildcard;
+                $certificate->price_approx = true;
+
+            } else {
+                $certificate->price = $rule->price;
+                $certificate->price_approx = true;
+            }
+
+        } elseif ($crt_ov){
+            if ($rule->price_ov) {
+                $certificate->price = $rule->price_ov;
+
+            } else {
+                $certificate->price = $rule->price;
+                $certificate->price_approx = true;
+            }
 
         } elseif ($crt_wild) {
             if ($rule->price_wildcard){
