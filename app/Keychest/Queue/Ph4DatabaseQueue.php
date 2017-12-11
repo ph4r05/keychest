6d676cc11a1f6e334d566039c43f636f4f29ab59
Dusan Klinec <dusan.klinec@gmail.com>
2017-12-11 12:40:55 +0100

    KCH-161: pd4database queue - delete directly, avoid deadlocks

--- app/Keychest/Queue/Ph4DatabaseQueue.php
+++ app/Keychest/Queue/Ph4DatabaseQueue.php
@@ -16,16 +16,21 @@ class Ph4DatabaseQueue extends DatabaseQueue implements QueueContract
      * Delete a reserved job from the queue.
      * https://github.com/laravel/framework/issues/7046
      *
-     * @param  string  $queue
-     * @param  string  $id
+     * @param  string $queue
+     * @param  string $id
      * @return void
+     * @throws \Exception|\Throwable
      */
     public function deleteReserved($queue, $id)
     {
         $this->database->transaction(function() use($queue, $id){
-            if ($this->database->table($this->table)->lockForUpdate()->find($id)) {
-                $this->database->table($this->table)->where('id', $id)->delete();
-            }
+            //if ($this->database->table($this->table)->lockForUpdate()->find($id)) {
+            //    $this->database->table($this->table)->where('id', $id)->delete();
+            //}
+
+            // Find, lock, delete mechanism is abandoned to avoid deadlocks.
+            // Trying to directly remove the job.
+            $this->database->table($this->table)->where('id', $id)->delete();
         }, 5);
     }
 
